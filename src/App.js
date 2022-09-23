e9a570fba36b7ecb1c44c46a6a55c0085ae7b716
ultimecreation <naserbechata@gmail.com>
2022-09-23 22:20:31 +0200

    retirve stored data after reload

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CreateEmployee from './components/CreateEmployee/CreateEmployee';
 import EmployeeList from './components/EmployeeList/EmployeeList';
+import { useContext, useEffect } from 'react';
+import { EmployeeContext } from './context/EmployeeContext';
 
 function App() {
+  const employeesContext = useContext(EmployeeContext)
+
+  useEffect(() => {
+    employeesContext.getStoredEmployees()
+  },[])
   return (
     <div className="App">
       <BrowserRouter>
