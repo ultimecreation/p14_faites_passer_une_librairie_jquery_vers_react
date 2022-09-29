6de90688cf248a8fc0a3db1616346326c2b35fd5
ultimecreation <naserbechata@gmail.com>
2022-09-29 23:52:23 +0200

    added pagination

--- src/components/TableBody/TableBody.js
+++ src/components/TableBody/TableBody.js
@@ -4,19 +4,21 @@ import './TableBody.css'
 const TableBody = ({ employees, columns }) => {
     return (
         <tbody>
-            {employees.map((employee, index) => {
-                return (
-                    <tr key={index}>
-                        {
-                            columns.map(column => {
-                                return <td key={column.accessor}>
-                                    {employee[column.accessor] ?? '__'}
-                                </td>
-                            })
-                        }
-                    </tr>
-                )
-            })
+            {employees.length > 0
+                ? employees.map((employee, index) => {
+                    return (
+                        <tr key={index}>
+                            {
+                                columns.map(column => {
+                                    return <td key={column.accessor}>
+                                        {employee[column.accessor] ?? '__'}
+                                    </td>
+                                })
+                            }
+                        </tr>
+                    )
+                })
+                :<tr ><td valign="top" colspan="9" class="dataTables_empty">No data available in table</td></tr>
             }
         </tbody>
     )
